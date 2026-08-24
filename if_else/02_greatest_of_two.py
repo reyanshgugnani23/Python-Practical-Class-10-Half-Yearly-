7ea00d784a054a1a1048c5092e2845217664ec6d
Reyansh Gugnani <reyanshgugnani8@gmail.com>
2026-08-24 20:16:31 +0530

    Add greatest of two program

---
+++ if_else/02_greatest_of_two.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+if num1 > num2:
+    print("The greatest number is:", num1)
+elif num2 > num1:
+    print("The greatest number is:", num2)
+else:
+    print("Both numbers are equal.")
