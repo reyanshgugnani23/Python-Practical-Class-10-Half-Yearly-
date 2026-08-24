29986e6daba09fb4f6dcf6407e644a8924590770
Reyansh Gugnani <reyanshgugnani8@gmail.com>
2026-08-24 20:16:16 +0530

    Add datatype examples

---
+++ examples/04_datatypes.py
@@ -0,0 +1,18 @@
+quantity = 10
+print(type(quantity))
+
+Price = -1921.9
+print(type(price))
+
+str1 = 'Hello Friend'
+str2 = "452"
+
+list1 = [5, 3.4, "New Delhi", "20C", 45]
+print(list1)
+
+tuple1 = (10, 20, "Apple", 3.4, 'a')
+print(tuple1)
+
+dict1 = {'Fruit':'Apple', 'Climate':'Cold', 'Price(kg)':120}
+print(dict1)
+print(dict1['Price(kg)'])
